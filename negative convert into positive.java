4b79b27f5c98e3f930dc28ce8c903253c05c3fac
Aswin2406 <90094057+Aswin2406@users.noreply.github.com>
2021-09-05 04:59:33 +0530

    Add files via upload

---
+++ negative convert into positive.java
@@ -0,0 +1,17 @@
+package week1day1;
+
+public class Pseudocode2 {
+	public static void main(String[] args) {
+		int num = 40;
+
+		
+		if (num > 0) {
+			System.out.println("it is positive number:"+num);
+			}
+			else if (num < 0) {
+				System.out.println("it is negative number:"+ num);
+			}
+		}
+	}
+
+
---
+++ negative nor positive.java
@@ -0,0 +1,31 @@
+package week1day1;
+
+public class Pseudocode {
+	public static void main(String[] args) {
+		int number =10;
+		
+		
+		
+		for (int i = 0; i <=0;i++ ) {
+			
+			if (number <=0 && number>=0) {
+				System.out.println("the number is neither positive nor negative:    "+number);
+			}
+		
+			
+			if (number>=0) {
+				System.out.println("it is positive number:    "+number);
+				
+			}
+		
+			else if (number<=0) {
+				System.out.println("it is negative number:    "+number);
+			}
+			
+			
+		}
+		
+		
+		}
+		
+}
